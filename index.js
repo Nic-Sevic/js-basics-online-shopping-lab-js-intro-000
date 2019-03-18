d52db9f5f0f1900c9e47e04d9f67f5da371fc926
Nic Sevic <nasevic@gmail.com>
2019-03-18 20:40:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,5 +69,5 @@ function placeOrder(cardNumber) {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
-  cart = []
+  return cart = []
 }
