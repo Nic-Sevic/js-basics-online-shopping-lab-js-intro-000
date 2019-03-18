5cec62bf8365d811f9e2354928c417a71474e8c4
Nic Sevic <nasevic@gmail.com>
2019-03-18 20:52:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   else {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    return (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    var totalCart = total()
+    cart = []
+    return (`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`)
   }
-  return cart = []
 }
